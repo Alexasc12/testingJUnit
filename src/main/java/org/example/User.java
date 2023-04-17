0e2095bd35e465e10b3d887a55e92526a2bcb67b
Alexasc <923248@mail.ru>
2023-04-18 06:45:18 +0700

    дз

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,6 +1,57 @@
 package org.example;
 
 public class User {
+    private String login;
+    private String email;
 
+    public User(String login, String email) {
+        if(login == null && email == null)
+                throw new NullPointerException();
+        else getInstance( login, email);
+
+        this.login = login;
+        this.email = email;
+    }
+
+
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public User() {
+          }
+
+//    public class MyClass {
+//        private int i;
+//        private MyClass(int i) {
+//            this.i=i;
+//        }
+    public   boolean getInstance(String login,String email) {
+            if (!checkCompereEmailAndLogin(login,email ) && !checkEmail(email)) {
+                throw new IllegalArgumentException( "Данные введены не коректно");
+            }
+            else return true;
+        }
+
+
+    public    boolean checkEmail(String email) {
+        if (email.contains("@")) {
+            return false;
+        } else{
+            return true;
+        }
+
+    }
+
+    public    boolean checkCompereEmailAndLogin(String login,String email) {
+        if (!email.equals(login)) {
+            return true;
+        }
+        else return false;
+    }
 
 }
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -5,6 +5,60 @@ import org.junit.jupiter.api.function.Executable;
 
 public class UserTest {
 
+//    Создайте тест, который выполнит тестирование создания объекта User с передачей в него двух параметров.
+//    Такая функция лишь должна проверять устанавливаются ли данные при создании объекта;
+
+//    Создайте аналогичный тест, который выполнит тестирование по созданию объекта без передачи в него параметров;
+
+//    Создайте тест, который протестирует установлен ли корректный Email адрес в поле email в классе User.
+//    Некорректным будет считаться тот email, в котором нет знака @ или же знака точки.
+
+    //    Создайте тест, который определяет, равны ли login и email (Они не должны быть равны).
+    private final User out = new User();
+
+    @Test
+    public void checkUserWithParameters() {
+        User out = new User("user", "123@mail.com");
+        Assertions.assertEquals("user", out.getLogin());
+        Assertions.assertEquals("123@mail.com", out.getEmail());
+    }
+
+    @Test
+    public void checkUserWitoutParametr() {
+        User out = new User();
+        Assertions.assertEquals(null, out.getLogin());
+        Assertions.assertEquals(null, out.getEmail());
+    }
+
+    @Test
+    public void checkEmail () {
+
+        String email1 = "email";
+        String email = "email@";
+        Assertions.assertFalse(out.checkEmail(email));
+        Assertions.assertTrue(out.checkEmail(email1));
+    }
+    @Test
+    public void checkCompereLoginAndEmail () {
+        String login = "login";
+        String email = "email@";
+        String login1 = "email@";
+        String email1 = "email@";
+        Assertions.assertTrue(out.checkCompereEmailAndLogin(login,email));
+        Assertions.assertFalse(out.checkCompereEmailAndLogin(login1,email1));
+
+    }
+
+    @Test
+    public void testGetInstanceInvalidInput() {
+        String login1 = "email@";
+        String email1 = "email@";
+        IllegalArgumentException exception =
+                Assertions.assertThrows(IllegalArgumentException.class,()->{ out.getInstance(login1,email1);
+                });
+        Assertions.assertEquals("Данные введены не коректно",exception.getMessage());
+    }
+
 
 }
 
